01fc230533f0ae37faf5ccf92637da0b1cb6e496
Mihai <mandoni@dynamitt.eu>
2020-03-12 17:59:56 +0200

    first commit

--- src/Tour.js
+++ src/Tour.js
@@ -165,25 +165,23 @@ function Tour({
 
   function nextStep() {
     setCurrent(prev => {
-      
-     let value =  (prev < steps.length - 1 ? prev + 1 : prev);
-       getCurrentStep(value);
-      return value;
-    };
-   
+      let value = prev < steps.length - 1 ? prev + 1 : prev
+      getCurrentStep(value)
+      return value
+    })
   }
 
   function prevStep() {
     setCurrent(prev => {
-      let value = (prev > 0 ? prev - 1 : prev);
-     getCurrentStep(value);
-      return value;
-    }
+      let value = prev > 0 ? prev - 1 : prev
+      getCurrentStep(value)
+      return value
+    })
   }
 
   function goTo(step) {
-    setCurrent(step);
-    getCurrentStep(step);
+    setCurrent(step)
+    getCurrentStep(step)
   }
 
   async function showStep(nextStep) {
